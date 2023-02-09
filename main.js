f77323bcf818b119fa0c6042f7c37ace9fbed50a
medinapicoli <mmedinabjj@gmail.com>
2023-02-09 09:39:36 -0300

    commit --amend

---
+++ main.js
@@ -0,0 +1,6 @@
+const form=document.getElementById('form')
+const firstName=document.getElementById('first-name')
+const lastName=document.getElementById('last-name')
+const emailAddress=document.getElementById('email-address')
+const password=document.getElementById('Password')
+
