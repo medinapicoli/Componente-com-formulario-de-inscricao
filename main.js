68ffb2ff96cd803f704738b80dd543e73329cb86
medinapicoli <mmedinabjj@gmail.com>
2023-02-12 23:35:37 -0300

    código base form

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
-const form=document.getElementById('form')
-const firstName=document.getElementById('first-name')
-const lastName=document.getElementById('last-name')
-const emailAddress=document.getElementById('email-address')
-const password=document.getElementById('Password')
+const firstName = document.querySelector('#first-name')
+const lastName = document.querySelector('#last-name')
+const emailAddress = document.querySelector('#email-address')
+const password = document.querySelector('#Password')
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validateInput();
+}); 
+
+const validateInput = () =>{
+const firstNameValue = firstName.value.trim()
+const lastNameValue = lastName.value.trim()
+const emailAddressValue = emailAddress.value.trim()
+const passwordValue = password.value.trim()
+    
+
+    if(firstNameValue ===''){
+        setErrorFor(firstName, 'First Name cannot be empty')
+        changeColor()
+    } else {
+        setSucessFor(firstName)
+    }
+
+    if(lastNameValue ===''){
+        setErrorFor(lastNameValue, 'Last Name cannot be empty')
+    } else {
+        setSucessFor(lastName)
+    }
+
+    if(emailAddressValue ===''){
+        setErrorFor(emailAddress, 'Looks like this is not an email')
+    } else {
+        setSucessFor(emailAddress)
+    }
+
+    if(passwordValue ===''){
+        setErrorFor(password, 'Password cannot be empty')
+    } else {
+        setErrorFor(password)
+    }
+}
+
+
 
